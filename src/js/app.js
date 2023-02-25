36fc53eae43b7e5b37ad443e67760ec019aaa0d5
mason.hows11 <mason.hows11@gmail.com>
2023-02-25 19:13:47 +0330

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// custom scripts
+function openNav() {
+    //document.getElementById('mySidenav').style.display = "block";
+    document.getElementById("my-side-nav").style.width = "280px";
+}
+
+function closeNav() {
+    //document.getElementById('mySidenav').style.display = "none";
+    document.getElementById("my-side-nav").style.width = "0";
+}
+
